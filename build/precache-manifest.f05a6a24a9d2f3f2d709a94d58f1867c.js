7aae369e1241c73de94d7891578d3f723faa0ea0
Wei Xiang <boyboi86@users.noreply.github.com>
2020-01-17 17:36:40 +0800

    Add files via upload

---
+++ build/precache-manifest.f05a6a24a9d2f3f2d709a94d58f1867c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850476026c6de8b4a3c7f49c7458e3d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52cb7a7c349da0583ce8",
+    "url": "/static/css/main.95f23ca2.chunk.css"
+  },
+  {
+    "revision": "a839ed3ae2ef885447a9",
+    "url": "/static/js/2.acae4427.chunk.js"
+  },
+  {
+    "revision": "9beab4c3b64f50a8086aaf710429ec50",
+    "url": "/static/js/2.acae4427.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52cb7a7c349da0583ce8",
+    "url": "/static/js/main.c44fd548.chunk.js"
+  },
+  {
+    "revision": "696942293a92ac3a2bb6",
+    "url": "/static/js/runtime-main.889ad4b6.js"
+  }
+]);
