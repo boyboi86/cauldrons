96412831a0f99bb9db81b3c4ff22618946096c7c
Wei Xiang <boyboi86@users.noreply.github.com>
2020-04-03 14:41:20 +0800

    Updates

---
+++ precache-manifest.373dfca0256f019386cecf0276279a57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b2b643784ed26dc8a0d62f3b81bfe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a55d4906d0e6673c79",
+    "url": "/static/css/2.ff4cff7f.chunk.css"
+  },
+  {
+    "revision": "d48873fd242b93998189",
+    "url": "/static/css/main.d6368958.chunk.css"
+  },
+  {
+    "revision": "e6a55d4906d0e6673c79",
+    "url": "/static/js/2.c59f84f8.chunk.js"
+  },
+  {
+    "revision": "205b84511a22a9a4e8c6094e87a4e682",
+    "url": "/static/js/2.c59f84f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d48873fd242b93998189",
+    "url": "/static/js/main.800bd0fa.chunk.js"
+  },
+  {
+    "revision": "91eced6c370591ab9268",
+    "url": "/static/js/runtime-main.a008825f.js"
+  }
+]);
